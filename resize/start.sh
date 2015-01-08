9820e51ce35048cf96763fd39a4f9007a1e8fb9e
Marko Pahić <mpahic@users.noreply.github.com>
2015-01-08 09:28:23 +0100

    Update start.sh

--- resize/start.sh
+++ resize/start.sh
@@ -1,3 +1,49 @@
-#!/bin/bash
+#!/bin/sh
 
-java -jar transcoder.jar
+DESC="Description"
+NAME=transcoder
+PIDFILE=.pid
+RUN_AS=root
+COMMAND=/usr/bin/java -- -jar transcoder.jar
+
+d_start() {
+  if [ -e $PIDFILE ]
+    then
+      echo "Application already started"
+    else   
+      start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --chuid $RUN_AS --exec $COMMAND
+  fi
+}
+
+d_stop() {
+    start-stop-daemon --stop --quiet --pidfile $PIDFILE
+    if [ -e $PIDFILE ]
+        then rm $PIDFILE
+    fi
+}
+
+case $1 in
+    start)
+      echo -n "Starting $DESC: $NAME"
+      d_start
+      echo "."
+      ;;
+    stop)
+      echo -n "Stopping $DESC: $NAME"
+      d_stop
+      echo "."
+      ;;
+    restart)
+      echo -n "Restarting $DESC: $NAME"
+      d_stop
+      sleep 1
+      d_start
+      echo "."
+      ;;
+    *)
+      echo "usage: $NAME {start|stop|restart}"
+      exit 1
+      ;;
+esac
+
+exit 0
